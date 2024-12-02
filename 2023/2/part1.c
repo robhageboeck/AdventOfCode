c38c807827cc05a685a6091082c96ae890cfa14e
Rob Hageboeck <rhageboeck@outlook.com>
2024-12-01 23:08:45 -0500

    No Time in 2023 :/

---
+++ 2023/2/part1.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+#define TOTAL_RED 12
+#define TOTAL_GREEN 13
+#define TOTAL_BLUE 14
+
+#define FILE "./2023/2/input.txt"
+
+void main() {
+  printf("Total: %d\n", TOTAL_RED + TOTAL_GREEN + TOTAL_BLUE);
+}
